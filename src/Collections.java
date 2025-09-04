d4a82d9a99091fd00425adf7a52bb0ce4812ad76
Alizaa <alizanoor035@gmail.com>
2025-09-05 02:44:04 +0500

    Add Collections (ArrayList demo)

---
+++ src/Collections.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+import  java.util.List;
+public class Collections {
+    public static void main(String[] args) {
+        List<Integer> list = new ArrayList<>();
+        List<Integer> list2 = new ArrayList<>();
+        list2.add(34);
+        list2.add(35);
+        list2.add(55);
+        System.out.println(list2);
+    }
+}
